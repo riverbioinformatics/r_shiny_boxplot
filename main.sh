de18a13c2798fff1bfe3002093d0986c2fe350e0
Thanh-Giang Nguyen <nttg8100@gmail.com>
2024-06-16 17:14:30 +0700

    Add fully support river computing

--- main.sh
+++ main.sh
@@ -1,1 +1,12 @@
-singularity exec R.sif R -e "shiny::runApp('/srv/shiny-server/app.R', host='0.0.0.0', port=$PORT)"
+if [ -d $NXF_SINGULARITY_CACHEDIR ]; then
+    echo "R.sif already exists"
+else
+    echo "Not found NXF_SINGULARITY_CACHEDIR global variable"
+    exit 1
+fi
+if [ -f $NXF_SINGULARITY_CACHEDIR/R.sif ]; then
+    echo "R.sif already exists"
+else
+    singularity build --fakeroot $NXF_SINGULARITY_CACHEDIR/R.sif singularity.def
+fi
+singularity exec $NXF_SINGULARITY_CACHEDIR/R.sif R -e "shiny::runApp('/srv/shiny-server/app.R', host='0.0.0.0', port=$PORT)"

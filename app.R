8149197e45afcd5e83f39cb0735f6c5e4aa65928
Thanh-Giang Nguyen <nttg8100@gmail.com>
2024-07-31 22:13:42 +0700

    Add raw app

--- app.R
+++ app.R
@@ -3,20 +3,10 @@ library(shiny)
 library(ggplot2)
 library(readr)
 
-
-user_base <- tibble::tibble(
-  user = c("user1"),
-  password = c("pass1"),
-  permissions = c("admin"),
-  name = c("River Computing")
-)
-
 # Define UI
 ui <- fluidPage(
-  div(class = "pull-right", shinyauthr::logoutUI(id = "logout")),
-  shinyauthr::loginUI(id = "login"),
   titlePanel("Interactive ggplot2 Visualization"),
-
+  
   sidebarLayout(
     sidebarPanel(
       fileInput("file1", "Choose CSV File",
@@ -37,23 +27,9 @@ ui <- fluidPage(
 
 # Define server logic
 server <- function(input, output, session) {
-  credentials <- shinyauthr::loginServer(
-    id = "login",
-    data = user_base,
-    user_col = user,
-    pwd_col = password,
-    log_out = reactive(logout_init())
-  )
-
-  # call the logout module with reactive trigger to hide/show
-  logout_init <- shinyauthr::logoutServer(
-    id = "logout",
-    active = reactive(credentials()$user_auth)
-  )
-
+  
   # Reactive expression to read the uploaded file
   datasetInput <- reactive({
-    req(credentials()$user_auth)
     req(input$file1)
     inFile <- input$file1
     
@@ -63,7 +39,6 @@ server <- function(input, output, session) {
   
   # Reactive expression to return the dataset
   data <- reactive({
-    req(credentials()$user_auth)
     if (is.null(input$file1)) {
       iris
     } else {
@@ -81,9 +56,7 @@ server <- function(input, output, session) {
   
   # Render plot based on user inputs
   output$plot <- renderPlot({
-    req(credentials()$user_auth)
     req(input$x_var, input$y_var)
-    req(credentials()$user_auth)
     ggplot(data(), aes_string(x = input$x_var, y = input$y_var)) +
       geom_boxplot() +
       theme_minimal() +
